c3f7d337b8f85cf2f7ed3a88f18ce2cb0a73f2f2
l3z <hi@l3z.io>
2023-03-14 21:58:16 +0800

    fix code style

--- api_proxy.go
+++ api_proxy.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/exec"
 	"flag"
-	"strconv"
+	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
+	"os/exec"
+	"strconv"
 )
 
 const (
@@ -16,17 +15,17 @@ const (
 )
 
 var (
-	port int
+	port   int
 	damaen bool
 )
 
-func init()  {
+func init() {
 	flag.IntVar(&port, "port", 8080, "监听端口")
 	flag.BoolVar(&damaen, DAEMON, false, "是否后台运行")
 }
 
 func ReverseProxyHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("[*] receive a request from %s, request header: %s: \n", r.RemoteAddr, r.Header)
+	log.Printf("[*] receive a request from %s, request header: %s: \n", r.RemoteAddr, r.Header)
 	target := "api.openai.com"
 	director := func(req *http.Request) {
 		req.URL.Scheme = "https"
@@ -35,7 +34,7 @@ func ReverseProxyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	proxy := &httputil.ReverseProxy{Director: director}
 	proxy.ServeHTTP(w, r)
-	fmt.Printf("[*] receive the destination website response header: %s\n", w.Header())
+	log.Printf("[*] receive the destination website response header: %s\n", w.Header())
 }
 
 func StripSlice(slice []string, element string) []string {
@@ -58,21 +57,21 @@ func SubProcess(args []string) *exec.Cmd {
 	cmd.Stderr = os.Stderr
 	err := cmd.Start()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[-] Error: %s\n", err)
+		log.Printf("[-] Error: %s\n", err)
 	}
 	return cmd
 }
 
 func main() {
 	flag.Parse()
-	fmt.Printf("[*] PID: %d PPID: %d ARG: %s\n", os.Getpid(), os.Getppid(), os.Args)
+	log.Printf("[*] PID: %d PPID: %d ARG: %s\n", os.Getpid(), os.Getppid(), os.Args)
 	if damaen {
 		SubProcess(StripSlice(os.Args, "-"+DAEMON))
-		fmt.Printf("[*] Daemon running in PID: %d PPID: %d\n", os.Getpid(), os.Getppid())
+		log.Printf("[*] Daemon running in PID: %d PPID: %d\n", os.Getpid(), os.Getppid())
 		os.Exit(0)
 	}
-	fmt.Printf("[*] Forever running in PID: %d PPID: %d\n", os.Getpid(), os.Getppid())
-	fmt.Printf("[*] Starting server at port %v\n", port)
+	log.Printf("[*] Forever running in PID: %d PPID: %d\n", os.Getpid(), os.Getppid())
+	log.Printf("[*] Starting server at port %v\n", port)
 	if err := http.ListenAndServe(":"+strconv.Itoa(port), http.HandlerFunc(ReverseProxyHandler)); err != nil {
 		log.Fatal(err)
 	}
